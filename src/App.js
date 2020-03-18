956aacdde103d22eecc7bab4f1dd09c944abf12d
Paul Boudreaux <paul@picayune.co>
2020-03-18 16:06:36 -0700

    Header and footer, infinite animation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import * as mockData from './mock/cards.json';
 import CardList from "./components/CardList";
+import NavBar from "./components/NavBar";
+import NavFoot from "./components/NavFoot";
 
 function App() {
   console.log(mockData.cards);
 
   return (
     <div className="App">
-      <CardList cardList={mockData.cards}/>
+        <NavBar/>
+        <CardList cardList={mockData.cards}/>
+        <NavFoot/>
     </div>
   );
 }
---
+++ src/components/NavFoot.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Nav.css';
+import './NavFoot.css';
+
+const NavFoot = props => (
+    <nav class="footNav"></nav>
+);
+
+export default NavFoot;
